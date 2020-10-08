b5b0098c10f6314757afe746bc3adb96be281789
kylelambert001 <kylelambert001@gmail.com>
2020-10-08 16:24:36 +1030

    Downloaded and setup Strapi CMS. Added collections for supplements and categories. Created all supplement listings with images attached.

---
+++ server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'be39415b2838111cf5bb90139e3307b6'),
+    },
+  },
+});
---
+++ server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8ad36215-ee0c-408c-bb7b-e0135c5a040f'
+};
