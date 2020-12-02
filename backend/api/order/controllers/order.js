893886099f3e2936d430421000e1b2c9093f411f
kylelambert001 <kylelambert001@gmail.com>
2020-12-02 23:11:19 +1030

    Revert order contoller changes

--- backend/api/order/controllers/order.js
+++ backend/api/order/controllers/order.js
@@ -4,6 +4,7 @@ const stripe = require("stripe")(
   "sk_test_51Hlmr7JNvmgYa7E8O5vHj5LIQQO2k9eN4qAEmNZjovwIVNzd0tz3vIxNrzVa4PSb6v3osizgBJ0dypqW57TNEWy200q7lYpelX"
 );
 
+const YOUR_DOMAIN = "http://localhost:3000/checkout";
 /**
  * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
  * to customize this controller
@@ -16,15 +17,12 @@ module.exports = module.exports = {
       const lineItems = [];
 
       shoppingCart.forEach((item) => {
-        const productImage =
-          item.options.flavour.image.formats.small.url || null;
         lineItems.push({
           price_data: {
             currency: "aud",
             product_data: {
               name: item.product.name,
               description: `Flavour: ${item.options.flavour.name}`,
-              images: [productImage],
             },
             unit_amount: item.product.price * 100,
           },
@@ -41,8 +39,8 @@ module.exports = module.exports = {
       line_items: [...lineItems],
       mode: "payment",
 
-      success_url: `${process.env.CLIENT_BASE_URL}/checkout?success=true`,
-      cancel_url: `${process.env.CLIENT_BASE_URL}/checkout?canceled=true`,
+      success_url: `${YOUR_DOMAIN}?success=true`,
+      cancel_url: `${YOUR_DOMAIN}?canceled=true`,
     });
 
     return { id: session.id };
