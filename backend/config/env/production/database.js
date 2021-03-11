188c94d3a74c9cd52762ffc7cfd5ee6cdcfee634
kylelambert001 <kylelambert001@gmail.com>
2021-03-11 23:09:55 +1030

    Update database config

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -13,9 +13,12 @@ module.exports = ({ env }) => ({
         database: config.database,
         username: config.user,
         password: config.password,
+        ssl: {
+          rejectUnauthorized: false,
+        },
       },
       options: {
-        ssl: false,
+        ssl: true,
       },
     },
   },
